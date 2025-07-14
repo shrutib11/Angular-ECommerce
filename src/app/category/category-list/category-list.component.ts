a91d4b1ef7715af56d506febcaf0ff2da79644ad
ShrutiTatva <tatva.pcq105@outlook.com>
2025-07-14 14:48:29 +0530

    Add Category with validation and sweet alert

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,37 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoryCardComponent } from '../category-card/category-card.component';
 import { CommonModule } from '@angular/common';
 import { Category } from '../../models/category.model';
 import { CategoryService } from '../../services/category.service';
 import Swal from 'sweetalert2';
+import { CategoryAddEditComponent } from '../category-add-edit/category-add-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-category-list',
-  imports: [CategoryCardComponent, CommonModule],
+  imports: [CategoryCardComponent, CommonModule, CategoryAddEditComponent, ReactiveFormsModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
   isLoading = true;
+  showAddModal: boolean = false;
   categories: Category[] = [];
 
-  constructor(private categoryService : CategoryService){}
+  constructor(private categoryService: CategoryService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    this.isLoading = true;
     this.categoryService.getAllCategories().subscribe({
-      next : (response) => {
-        this.categories = response.result;
+      next: (response) => {
+        this.categories = response.result || [];
         this.isLoading = false;
       },
-      error: (err) => {
-        this.isLoading = false;
+      error: (error) => {
         Swal.fire({
           icon: 'error',
           title: 'Oops...',
-          text: err,
+          text: error.statusText,
           confirmButtonColor: '#d33'
         });
+        this.isLoading = false;
       }
-    })
+    });
+  }
+
+  openAddModal(): void {
+    this.showAddModal = true;
+  }
+
+  closeAddModal(): void {
+    this.showAddModal = false;
+  }
+
+  onCategoryAdded(newCategory : Category): void {
+    this.categories.push(newCategory);;
   }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../models/api-response.model';
 import { Category } from '../models/category.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  private baseUrl = 'http://localhost:7000/category';
+  private baseUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
   getAllCategories() : Observable<ApiResponse<Category[]>> {
     return this.http.get<ApiResponse<Category[]>>(`${this.baseUrl}/GetAll`)
+  }
+
+  addCategory(categoryData : FormData) : Observable<ApiResponse<Category>> {
+    return this.http.post<ApiResponse<Category>> (`${this.baseUrl}/Add`, categoryData)
   }
 }
